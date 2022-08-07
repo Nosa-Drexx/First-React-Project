140ce7c3e11d61c72f2cf9e4ef023bd0203385d3
nosa <nosaegharevba01@gmail.com>
2022-08-07 20:07:39 +0100

    add a winning effect

--- src/winnerCheck.js
+++ src/winnerCheck.js
@@ -13,52 +13,67 @@ export default function winnerCheck(
     characterHolder[1].innerHTML === Character &&
     characterHolder[2].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 0, 1, 2);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[0].innerHTML === Character &&
     characterHolder[3].innerHTML === Character &&
     characterHolder[6].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 0, 3, 6);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[0].innerHTML === Character &&
     characterHolder[4].innerHTML === Character &&
     characterHolder[8].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 0, 4, 8);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[2].innerHTML === Character &&
     characterHolder[4].innerHTML === Character &&
     characterHolder[6].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 2, 4, 6);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[2].innerHTML === Character &&
     characterHolder[5].innerHTML === Character &&
     characterHolder[8].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 2, 5, 8);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[6].innerHTML === Character &&
     characterHolder[7].innerHTML === Character &&
     characterHolder[8].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 6, 7, 8);
     return sidePiece(load,who,whoScored);
   } else if (
-    characterHolder[1].innerHTML === Character &&
     characterHolder[4].innerHTML === Character &&
+    characterHolder[1].innerHTML === Character &&
     characterHolder[7].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 4, 1, 7);
     return sidePiece(load,who,whoScored);
   } else if (
     characterHolder[3].innerHTML === Character &&
     characterHolder[4].innerHTML === Character &&
     characterHolder[5].innerHTML === Character
   ) {
+    winnerColorAndEffect(characterHolder, 3, 4, 5);
     return sidePiece(load,who,whoScored);
   }
  
   }
+
+  function winnerColorAndEffect(characterHolder, position1, position2, position3){
+    characterHolder[position1].style.backgroundColor = "rgba(255, 255, 0, 0.5)";
+    characterHolder[position2].style.backgroundColor = "rgba(255, 255, 0, 0.5)";
+    characterHolder[position3].style.backgroundColor = "rgba(255, 255, 0, 0.5)";
+  }
+  
   return whoWon(userCharacter,"You","UserScore") || whoWon(computerCharacter,"AI", "AiScore") || false;
 }
 
